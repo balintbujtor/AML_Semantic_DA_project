a03e655e783fdd32eccf690834a9683a24bfdaa2
boyan <be.cieutat@protonmail.com>
2024-04-28 12:51:37 +0200

    train.py import modif

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from pathlib import Path
 from torch.utils.data import DataLoader
 from utils import *
-from AML_Semantic_DA_project.trainings.train_1 import train, val
+from trainings.train_1 import train, val
 
 def main():
     args = parse_args()
