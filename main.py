7bdd85c1fddb4610fc2c6b6a9fbaec4ba6d35c3b
Bálint Bujtor <balint.bujtor@gmail.com>
2024-05-14 19:12:04 +0200

    removed the code for calculation mean and std on cityscapes

--- main.py
+++ main.py
@@ -11,29 +11,7 @@ import trainings.train_1 as train_1
 import trainings.train_ADA as train_ADA
 import trainings.train_FDA as train_FDA
 import datasets.augment as augment
- 
-def calculate_mean_std(dataloader):
-    """Calculates the mean and std of the dataset
 
-    Args:
-        dataset (CityScapes): the dataset to calculate the mean and std of
-
-    Returns:
-        tuple: the mean and std of the dataset
-    """
-    mean = 0.
-    std = 0.
-    nb_samples = 0.
-    for data, _ in dataloader:
-        batch_samples = data.size(0)
-        data = data.view(batch_samples, data.size(1), -1)
-        mean += data.mean(2).sum(0)
-        std += data.std(2).sum(0)
-        nb_samples += batch_samples
-
-    mean /= nb_samples
-    std /= nb_samples
-    return mean, std
 
 def main():
     args = parse_args()
@@ -66,27 +44,7 @@ def main():
     STD = [0.229, 0.224, 0.225]
     
     assert args.training_dataset in ['','cityscapes', 'gta5'], "Dataset not supported"
-    assert val_dataset in ['','cityscapes', 'gta5'], "Dataset not supported"
-    
-    # TODO: REMOVE AFTER COMPUTING CS MEAN AND STD
-    img_transforms = v2.Compose([
-        v2.ToTensor(),
-    ])
-    dataset = CityScapes(root_dir="Cityscapes/Cityspaces/", split='train', img_transforms=img_transforms, lbl_transforms=img_transforms)
-    dataloader_train = DataLoader(dataset,
-                                batch_size=args.batch_size,
-                                shuffle=True,
-                                num_workers=args.num_workers,
-                                pin_memory=False,
-                                drop_last=True)
-    
-    mean, std = calculate_mean_std(dataloader_train)
-    
-    print(f"Mean: {mean}, Std: {std}")
-    print('Entering while because mean and std are computed')
-
-    while True:
-        pass   
+    assert val_dataset in ['','cityscapes', 'gta5'], "Dataset not supported" 
     
      
     #Loads cityscapes if it's used in train or val
