32822c7dd18de6d309f78d868d1c756ef8428bed
Inaam El Helwe s306979 <s306979@studenti.polito.it>
2024-06-01 12:34:15 +0200

    debug

--- utils/fda.py
+++ utils/fda.py
@@ -241,10 +241,17 @@ def pseudo_label_gen(args,
 
             pred = (pred_1 + pred_2 + pred_3) / 3
             pred = torch.nn.functional.softmax(pred, dim=1)
-            
-            label, prob = torch.argmax(pred,axis=2), torch.max(pred,axis=2)
-            predicted_label[i] = torch.Tensor.copy_(label, False)
-            predicted_prob[i] = torch.Tensor.copy_(pred, False)
+
+            if pred.is_cuda:
+                pred = pred.cpu()
+
+            pred_label, prob = np.argmax(pred,axis=2), np.max(pred,axis=2)
+            predicted_label[i] = pred_label.copy_(False)
+            predicted_prob[i] = pred.copy_(False)
+
+            # pred_label, prob = torch.argmax(pred,axis=2), torch.max(pred,axis=2)
+            # predicted_label[i] = torch.Tensorcopy_(pred_label, False)
+            # predicted_prob[i] = torch.Tensor.copy_(pred, False)
 
             # compute per pixel accuracy
             precision = ut.compute_global_accuracy(pred, label)
