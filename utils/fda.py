e9eb1f855044a3866304b88c6c8327cbce2e3ea5
boyan <be.cieutat@protonmail.com>
2024-06-04 00:23:36 +0200

    debug pseudo label save

--- utils/fda.py
+++ utils/fda.py
@@ -316,6 +316,6 @@ def pseudo_label_gen(args,
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         
-        output.save('%s/%s' % (save_path, name)) 
+        output.save('%s/%s' % (save_path, os.path.basename(name))) 
 
     return
